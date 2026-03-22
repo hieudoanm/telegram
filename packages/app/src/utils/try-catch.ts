691348026d17041e8e95b07802cd03ad98b3fa75
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-22 17:00:55 +0700

    Update tryCatch

--- packages/app/src/utils/try-catch.ts
+++ packages/app/src/utils/try-catch.ts
@@ -11,7 +11,7 @@ type Failure<E> = {
 
 type Result<T, E = Error> = Success<T> | Failure<E>;
 
-// Main wrapper function
+// Main wrapper
 export const tryCatch = async <T, E = Error>(
   promise: Promise<T>
 ): Promise<Result<T, E>> => {
